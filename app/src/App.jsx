a86fd7a2d6348894109fda318a1a7a5f6c832356
Harsh Gupta <harshgupta82003@gmail.com>
2026-03-11 22:01:09 +0530

    feat : add Footer component

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import Navbar from './components/Navbar'
 import News from './Page/News'
 import Category from './components/Category'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,6 +14,7 @@ function App() {
   <Navbar/>
   <Category className = "py-10"/>
   <News/>
+  <Footer/>
    </>
   )
 }
